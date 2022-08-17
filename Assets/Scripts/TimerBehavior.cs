091a4774ce4c11f0c70594887ecb0a2047174560
Hassan-iqbal6 <43201426+Hassan-iqbal6@users.noreply.github.com>
2022-08-17 16:05:57 +0500

    new timer added

--- Assets/Scripts/TimerBehavior.cs
+++ Assets/Scripts/TimerBehavior.cs
@@ -2,22 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerBehavior : MonoBehaviour
 {
     [SerializeField] private TMP_Text textTitle;
     [SerializeField] private TMP_Text textTimer;
+    [SerializeField] private  Image fillImage;
+    int initialTime;
 
     public void SetupTimer(string title, int time)
     {
-        textTitle.text = title;
+        textTitle.text = title.ToUpper();
         textTimer.text = string.Format("{0:00}",time);
+        textTimer.fontSize = textTitle.fontSize;
+        fillImage.fillAmount = 1f;
+        initialTime = time;
         gameObject.SetActive(true);
     }
 
     public void UpdateTimer(int time)
     {
+        if(textTimer.fontSize!=textTitle.fontSize)
+            textTimer.fontSize = textTitle.fontSize;
         textTimer.text = string.Format("{0:00}",time);
+        fillImage.fillAmount = ((float)time / (float)initialTime);
         if(time == 0) StopTimer();
     }
 
